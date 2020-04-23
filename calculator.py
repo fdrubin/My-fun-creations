47d98caaae5483acd54e3b419fd00b9628cf76ff
Daniel Rubin <60769973+fdrubin@users.noreply.github.com>
2020-04-22 23:13:30 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+print("I have a boyfrienddddd Maddox! Also sorry for the delay - i kept forgetting.")
+num1 = float(input("Type in first number: "))
+op = (input("Type in operator or method of operation."))
+num2 = float(input("Type in second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+else:
+    print("Not operable")
